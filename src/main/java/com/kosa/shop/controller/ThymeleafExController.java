c35d372a480a1b45b4baef44b8854f5cc87c6f88
IceMan4U <iceman4u@naver.com>
2023-09-24 20:17:48 +0900

    add: 3.3.6 예제

--- src/main/java/com/kosa/shop/controller/ThymeleafExController.java
+++ src/main/java/com/kosa/shop/controller/ThymeleafExController.java
@@ -77,4 +77,12 @@ public class ThymeleafExController {
     public String thymeleafExample05() {
         return "thymeleafEx/thymeleafEx05";
     }
+
+    @GetMapping("/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+
+        return "thymeleafEx/thymeleafEx06";
+    }
 }
