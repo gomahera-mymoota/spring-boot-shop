5972a97a0acde134e8c725ff2be78265b74fd9b1
IceMan4U <iceman4u@naver.com>
2023-09-25 01:31:59 +0900

    add: 3.4.1 예제 - thymeleaf-layout-dialect의 버전을 3.3.0으로

--- src/main/java/com/kosa/shop/controller/ThymeleafExController.java
+++ src/main/java/com/kosa/shop/controller/ThymeleafExController.java
@@ -85,4 +85,9 @@ public class ThymeleafExController {
 
         return "thymeleafEx/thymeleafEx06";
     }
+
+    @GetMapping("/ex07")
+    public String thymeleafExample07() {
+        return "thymeleafEx/thymeleafEx07";
+    }
 }
