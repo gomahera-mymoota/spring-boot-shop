3d33148df184403a5542dc43add0b791111322fd
IceMan4U <iceman4u@naver.com>
2023-09-26 01:24:01 +0900

    add: log in, out 구현

--- src/main/java/com/kosa/shop/config/SecurityConfig.java
+++ src/main/java/com/kosa/shop/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.kosa.shop.config;
 
+import com.kosa.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,13 +9,32 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class SecurityConfig {
+    // WebSecurityConfigurerAdapter가 deprecated 되어서
+    // SecurityFilterChain 객체를 Bean으로 등록해야 함
 
+    @Autowired
+    MemberService memberService;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/");
+
+        return http.build();
     }
 
     @Bean
--- src/main/java/com/kosa/shop/service/MemberService.java
+++ src/main/java/com/kosa/shop/service/MemberService.java
@@ -4,6 +4,10 @@ import com.kosa.shop.entity.Member;
 import com.kosa.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +16,7 @@ import javax.validation.constraints.NotNull;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService extends UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -27,5 +31,18 @@ public class MemberService {
         if (foundMember.isPresent()) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var member = memberRepository.findByEmail(email);
+
+        var result = member.orElseThrow(() -> new UsernameNotFoundException(email));     // Optional 활용
+
+        return User.builder()
+                .username(result.getEmail())
+                .password(result.getPassword())
+                .roles(result.getRole().toString())
+                .build();
     }
 }
