9162894097d92973d448c3ddd6705d8f4855238c
IceMan4U <iceman4u@naver.com>
2023-09-26 23:02:31 +0900

    add: 5.1.1 예제

--- src/test/java/com/kosa/shop/entity/CartTest.java
+++ src/test/java/com/kosa/shop/entity/CartTest.java
@@ -1,14 +1,21 @@
 package com.kosa.shop.entity;
 
+import com.kosa.shop.dto.MemberFormDto;
 import com.kosa.shop.repository.CartRepository;
 import com.kosa.shop.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceContext;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -25,5 +32,37 @@ class CartTest {
     PasswordEncoder passwordEncoder;
 
     @PersistenceContext
-    EntityMananger em;
+    EntityManager em;
+
+    public Member createMember() {
+        var memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword("1234");
+
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("장바구니 회원 엔티티 매핑 조회 테스트")
+    public void findCartAndMemberTest() {
+        // given
+        var member = createMember();
+        memberRepository.save(member);
+
+        var cart = new Cart();
+        cart.setMember(member);
+        cartRepository.save(cart);
+
+        em.flush();
+        em.clear();
+
+        // when
+        var savedCart = cartRepository.findById(cart.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        // then
+        assertThat(savedCart.getMember().getId()).isEqualTo(member.getId());
+    }
 }
